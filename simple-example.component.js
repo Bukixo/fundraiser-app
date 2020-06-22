158b53cb8c2f3ffd1d5f97615ee3e823a45a4de5
Buki Thompson <bukithompson@Bukis-MacBook-Air.local>
2020-06-22 09:25:51 +0100

    adding a controller

---
+++ simple-example.component.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+
+    angular.module('app').component('simpleExample', {
+        controllerAs: 'vm',
+        controller: function () {
+            var vm = this;
+
+            vm.name = {
+                first: '',
+                last: ''
+            };
+
+            vm.fullName = '';
+
+            vm.submit = function () {
+                console.log(vm.name);
+                vm.fullName = vm.name.first + ' ' + vm.name.last;
+                console.log(vm.name.first, vm.name.helloStateast, vm.fullName);
+            }
+        }
+    });
+})();
